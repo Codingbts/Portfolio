09ad89eb04ddfaa15dfe0bfb502df12f45a03577
dev <azertwxcvbn259@gmail.com>
2025-01-29 03:22:59 +0100

    fond pour la page selection du portfolio

--- public/selection-page.php
+++ public/selection-page.php
@@ -9,20 +9,71 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,200;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,200;1,400;1,500;1,600;1,700;1,800;1,900&family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="selection-page.css">
+    <script src="https://cdn.jsdelivr.net/npm/particles.js"></script>
 </head>
 
 <body>
-<div class="container">
-        <h1>Selectionner le type de <span>portfolio</span></h1>
-        <div class="selection-wrapper">
-            <div class="classic-portfolio">
-                <a href="portfolio.php"><img src="images/classic-site.png" alt=""></a>
+<div id="overlay"><h1>Choisissez votre Portfolio</h1></div>
+<div id="hey"><div id="layer-up"></div></div>
+<div id="layer-0">
+  
+  <div id="layer-1">
+    <div id="layer-2">
+      <div id="lines">
+        <div id="layer-corner"></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <div class="container">
+        <div class="modes-container">
+            <div class="site">
+                <div class="mode classic" id="classic">
+                    <div class="screen">
+                        <a href="portfolio.php"><img src="images/classic-site.png" alt="site portfolio"></a>
+                    </div>
+
+                </div>
+                <div class="titre-info">
+                    <span>Site</span>
+                    <h3>Temps estimé : </h3>
+                </div>
             </div>
-            <div class="game-portfolio">
-                <a href=""><img src="images/portfolio-game.png" alt="game portfolio"></a>
+
+            <div class="jeu">
+                <div class="mode game" id="game">
+                    <div class="screen">
+                        <img src="images/portfolio-game.png" alt="portfolio-game">
+                    </div>
+
+                </div>
+                <div class="titre-info">
+                    <span>Jeu</span>
+                    <h3>Temps estimé : </h3>
+                </div>
             </div>
         </div>
     </div>
+
+    <audio id="hover-sound" src="hover-sound.mp3"></audio>
+
+    <script>
+        document.querySelectorAll('.mode').forEach(mode => {
+            mode.addEventListener('mouseenter', () => {
+                const sound = document.getElementById('hover-sound');
+                sound.play();
+            });
+
+            mode.addEventListener('click', (e) => {
+                if (e.target.id === 'classic') {
+                    window.location.href = "portfolio.php";
+                } else if (e.target.id === 'game') {
+                    window.location.href = "game-portfolio.php";
+                }
+            });
+        });
+    </script>
 </body>
 
 </html>
